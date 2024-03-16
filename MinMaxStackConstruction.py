3d4a7f552aa48076388d981c75c993c68a402ba2
kerem-z <e228876@metu.edu.tr>
2024-03-16 23:44:58 +0300

    Add MinMaxStack class implementation

---
+++ MinMaxStackConstruction.py
@@ -0,0 +1,35 @@
+class MinMaxStack:
+    def __init__(self):
+        self.stack = []
+        self.minMaxStack = []
+
+    def peek(self):
+        if self.stack: 
+            return self.stack[-1]
+
+    def pop(self):
+            if self.stack:
+                self.minMaxStack.pop()
+                return self.stack.pop()
+    
+    def push(self, value):
+         newMinMax = {'min': value, 'max': value}
+         if self.minMaxStack
+            lastMinMax = self.minMaxStack[-1]
+            newMinMax['min'] = min(lastMinMax['min'], value)
+            newMinMax['max'] = max(lastMinMax['max'], value)
+            self.stack.append(value)
+            self.minMaxStack.append(newMinMax)
+    def getMin(self):
+        if not self.minMaxStack:
+            return None
+         
+        return self.minMaxStack[-1]['min']
+    
+    def getMax(self):
+         if not self.minMaxStack:
+              return None
+         return self.minMaxStack[-1]['max']
+
+         
+                 
